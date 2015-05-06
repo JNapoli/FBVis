5b6bcc52dbe820ddab06e7785df7ad6f815ddd54
Joe Napoli <JNapoli@stanford.edu>
2015-05-06 12:32:56 -0700

    Parse original parameters

--- FBVis.py
+++ FBVis.py
@@ -1,20 +1,30 @@
 import fileinput
 import glob
+import linecache
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 
 class FBVis:
-    def __init__(self):
+    def __init__(self, target):
+        self.target = target
         self.prefix = self._get_prefix()
         self.it_data, self.parameters, self.experiment = self._parse(self.prefix)
 
     def _get_prefix(self):
+        """
+        There should be only one FB input file in the working directory.
+        """
         return glob.glob('*.in')[0].split('.')[0]
 
     def _parse(self, job_prefix):
         out_filenms = glob.glob('*.out')
         concat_filenm = self._concatenate(out_filenms)
+        
+        with open(concat_filenm, 'r') as f:
+            raw_dat = f.readlines()
+            params = self._parse_params(raw_dat)
+        f.close()
 
     def _concatenate(self, filenms):
         """
@@ -28,4 +38,25 @@ class FBVis:
         fout.close()
 
         return out_name
+
+    def _parse_params(self, raw_dat):
+        """
+        Parse parameter names and indeces. 
+        """
+        params = []
         
+        for i, line in enumerate(raw_dat):
+            if 'Starting parameter indices, physical values and IDs' in line:
+                parsed = False
+                current = i + 2
+
+                while not parsed:
+                    fields = raw_dat[current].split()
+
+                    if fields[0].isdigit():
+                        params.append((int(fields[0]), float(fields[2]), fields[-1]))
+                        current += 1
+                    else:        
+                        # Reached end of parameter block            
+                        parsed = True 
+        return params        
